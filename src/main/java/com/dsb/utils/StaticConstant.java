276b4cd138f5b3439b579a8d73eae62252ca662d
k741350093 <741350093@qq.com>
2016-07-27 10:33:06 +0800

    staticCOnstant

---
+++ src/main/java/com/dsb/utils/StaticConstant.java
@@ -0,0 +1,8 @@
+package com.dsb.utils;
+
+public class StaticConstant {
+	public static String appid="wxa6e1bbd8c3101e94";//第三方用户唯一凭证
+	public static String secret="b46bd37e33f913a528141e32286e27be";//第三方用户唯一凭证密钥
+	public static String jspFilePath="/WechatCard/src/main/webapp/WEB-INF/jsp/";
+	public static String cssFilePath="/WechatCard/src/main/webapp/WEB-INF/css/";
+}
