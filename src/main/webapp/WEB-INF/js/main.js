96fb961a8b083af9a5f5a0c615563446c792e7cf
FiseCoco <FiceCoco@163.com>
2016-08-05 15:29:03 +0800

    前端代码修改

--- src/main/webapp/WEB-INF/js/main.js
+++ src/main/webapp/WEB-INF/js/main.js
@@ -1,3 +1,60 @@
+function getDate(strDate) {        //将字符串转化为时间类型 
+        var date = eval('new Date(' + strDate.replace(/\d+(?=-[^-]+$)/,    
+         function (a) { return parseInt(a, 10) - 1; }).match(/\d+/g) + ')');    
+        return date;    
+    } 
+
+(function($) {
+	$.extend({
+	  myTime: {
+	    /**              
+	    * 日期 转换为 Unix时间戳
+	* @param <int> year    年              
+	* @param <int> month   月              
+	* @param <int> day     日              
+	* @param <int> hour    时              
+	* @param <int> minute  分              
+	* @param <int> second  秒              
+	* @return <int>        unix时间戳(秒)              
+	*/            
+	DateToUnix: function(year, month, day, hour, minute, second){                 
+	var oDate =
+	new Date(Date.UTC(parseInt(year),                         
+	parseInt(month), parseInt(day),                         
+	parseInt(hour), parseInt(minute),                         
+	parseInt(second)
+	)                 
+	);                 
+	return (oDate.getTime()/1000);
+	},             
+	/**              
+	* 时间戳转换日期              
+	* @param <int> unixTime    待时间戳(秒)              
+	* @param <bool> isFull    返回完整时间(Y-m-d 或者 Y-m-d H:i:s)              
+	* @param <int>  timeZone   时区              
+	*/
+	UnixToDate: function(unixTime, isFull, timeZone){                 
+	if (typeof(timeZone) == 'number')                 
+	{                     
+	unixTime = parseInt(unixTime) + parseInt(timeZone) * 60 * 60;                 
+	}
+	var time = new Date(unixTime*1000);                 
+	var ymdhis = "";
+	ymdhis += time.getUTCFullYear() + "-";                 
+	ymdhis += time.getUTCMonth() + "-";                 
+	ymdhis += time.getUTCDate();
+	if ( isFull === true )                 
+	{                     
+	ymdhis += " " + time.getUTCHours() + ":";                     
+	ymdhis += time.getUTCMinutes() + ":";                     
+	ymdhis += time.getUTCSeconds();                 
+	}
+	return ymdhis;             
+	}         
+	}     
+	}); 
+	})(jQuery); 
+
 $().ready(function(){
 	var ismoreCard = false;
 	var bodyH;
@@ -9,67 +66,7 @@ $().ready(function(){
 		"height": bodyH
 	});
 	
-	function getDate(strDate) {        //将字符串转化为时间类型 
-        var date = eval('new Date(' + strDate.replace(/\d+(?=-[^-]+$)/,    
-         function (a) { return parseInt(a, 10) - 1; }).match(/\d+/g) + ')');    
-        return date;    
-    } 
-
-(function($) {
-$.extend({ 
-  myTime: {             
-    /**
-     * 当前时间戳
-     * @return <int>    unix时间戳(秒) 
-     */
-    CurTime: function(){
-      return Date.parse(new Date())/1000;
-    },
-    /**       
-     * 日期 转换为 Unix时间戳
-     * @param <string> 2014-01-01 20:20:20 日期格式       
-     * @return <int>    unix时间戳(秒)       
-     */
-    DateToUnix: function(string) {   //  将时间类型转化为时间戳
-      var f = string.split(' ', 2);
-      var d = (f[0] ? f[0] : '').split('-', 3);
-      var t = (f[1] ? f[1] : '').split(':', 3);
-      return (new Date(
-          parseInt(d[0], 10) || null,
-          (parseInt(d[1], 10) || 1) - 1,
-          parseInt(d[2], 10) || null,
-          parseInt(t[0], 10) || null,
-          parseInt(t[1], 10) || null,
-          parseInt(t[2], 10) || null
-          )).getTime() / 1000;
-    },
-    /**       
-     * 时间戳转换日期       
-     * @param <int> unixTime  待时间戳(秒)       
-     * @param <bool> isFull  返回完整时间(Y-m-d 或者 Y-m-d H:i:s)       
-     * @param <int> timeZone  时区       
-     */
-    UnixToDate: function(unixTime, isFull, timeZone) {          //将时间戳转化为时间类型
-      if (typeof (timeZone) == 'number')
-      {
-        unixTime = parseInt(unixTime) + parseInt(timeZone) * 60 * 60;
-      }
-      var time = new Date(unixTime * 1000);
-      var ymdhis = "";
-      ymdhis += time.getUTCFullYear() + "-";
-      ymdhis += (time.getUTCMonth()+1) + "-";
-      ymdhis += time.getUTCDate();
-      if (isFull === true)
-      {
-        ymdhis += " " + time.getUTCHours() + ":";
-        ymdhis += time.getUTCMinutes() + ":";
-        ymdhis += time.getUTCSeconds();
-      }
-      return ymdhis;
-    }
-  }
-});
-})(jQuery);
+	
 	
 	$("#moreCard").click(function(event) {
 		if(ismoreCard == false){
